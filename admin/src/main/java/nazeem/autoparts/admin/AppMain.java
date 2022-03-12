8bea58b1b193403301741d5d6bb40d881754e882
Lucie Kana <81702035+LucieKana@users.noreply.github.com>
2022-03-12 16:23:15 -0500

    Add files via upload

---
+++ admin/src/main/java/nazeem/autoparts/admin/AppMain.java
@@ -0,0 +1,15 @@
+package nazeem.autoparts.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EntityScan("nazeem.autoparts.library.model")
+@EnableJpaRepositories("nazeem.autoparts.library.repository")
+public class AppMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class, args);
+    }
+}
